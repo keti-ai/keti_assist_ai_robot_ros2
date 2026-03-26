cbe75f825481ace8a7e7a64ed98b2f2f5ca9dd87
ferglie0812 <ferglie@naver.com>
2026-03-26 16:56:33 +0900

    updating kaair_apps

---
+++ src/kaair_apps/setup.py
@@ -0,0 +1,26 @@
+import os
+from glob import glob
+from setuptools import setup
+
+
+package_name = 'kaair_apps'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        (os.path.join('share', package_name), ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='keti',
+    maintainer_email='seomi@keti.re.kr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
